f9612c5a147cdc17b8fd344174ccecdc09402b3e
Ahmedk-n <ahmedkadryinfo@gmail.com>
2023-08-19 21:54:59 +0300

    added some stuff

--- backend/growfree/base/views.py
+++ backend/growfree/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models 
 
 # Create your views here.
 
@@ -7,4 +8,25 @@ def index(request):
     return render(request, 'index.html')
 
 def resources(request):
-    pass 
+    # context = {
+    #     "resources" : models.Opportunity.objects.all(),
+    #     "tags": models.Tag.objects.all(),
+    #     }
+    context = {
+    "resources": models.Opportunity.objects.all(),
+    "tags": models.Tag.objects.all(),
+}
+    tags_with_opportunities = []
+    for tag in context["tags"]:
+        tag_with_opportunities = {
+            "tag": tag,
+            "opportunities": tag.opportunities.all(),
+        }
+        tags_with_opportunities.append(tag_with_opportunities)
+    context["tags_with_opportunities"] = tags_with_opportunities
+    return render(request , "opportunities.html" , context) 
+
+
+## Form  
+def research_help(request):
+    pass
